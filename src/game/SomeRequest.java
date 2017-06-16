fb5d3317ab20747ddfe313f1e6b9aef31efdbd3a
Coud <nikielprzemek@gmail.com>
2017-06-16 22:36:55 +0200

    initial commit

---
+++ src/game/SomeRequest.java
@@ -0,0 +1,5 @@
+package game;
+
+public class SomeRequest {
+    public String text;
+}
---
+++ src/game/SomeResponse.java
@@ -0,0 +1,8 @@
+package game;
+
+/**
+ * Created by coud on 16.06.17.
+ */
+public class SomeResponse {
+    public String text;
+}
