858a563a90f9d4b4fcb80a3eed541c045a05ac4a
Hubert Kurek <hkurkov@gmail.com>
2017-06-17 19:10:12 +0200

    Fixed magic numbers, added CardButton to BoardPanel

--- src/game/client/board/BoardPanel.java
+++ src/game/client/board/BoardPanel.java
@@ -5,12 +5,20 @@ import javax.swing.JPanel;
 import java.awt.GridLayout;
 
 public class BoardPanel extends JPanel {
+    public static final int MAX_BUTTONS = 20;
+    public static final int ROWS = 4;
+    public static final int COLS = 5;
+
     public BoardPanel() {
         initPanel();
     }
 
     private void initPanel() {
-        setLayout(new GridLayout(4, 5));
+        setLayout(new GridLayout(ROWS, COLS));
         setBorder(BorderFactory.createTitledBorder("Board"));
+
+        for (int i = 0; i < MAX_BUTTONS; i++) {
+            add(new CardButton());
+        }
     }
 }
